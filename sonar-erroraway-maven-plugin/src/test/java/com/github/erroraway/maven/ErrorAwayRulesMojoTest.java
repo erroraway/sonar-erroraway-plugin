dacee0f8b672fcea024b8845b83498245a12e197
gtoison <guillaume.toison@tobam.fr>
2024-12-12 10:43:42 +0100

    test: check the maven plugin failure cases
    - Added the documentation folder to the test classpath so the plugin can
    generate metadata during tests
    - Test the case when a rule description fails to load

--- sonar-erroraway-maven-plugin/src/test/java/com/github/erroraway/maven/ErrorAwayRulesMojoTest.java
+++ sonar-erroraway-maven-plugin/src/test/java/com/github/erroraway/maven/ErrorAwayRulesMojoTest.java
@@ -1,8 +1,16 @@
 package com.github.erroraway.maven;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.io.File;
+import java.net.URL;
 
 import org.junit.jupiter.api.Test;
 
@@ -12,7 +20,19 @@ class ErrorAwayRulesMojoTest {
 	void execute() {
 		ErrorAwayRulesMojo mojo = new ErrorAwayRulesMojo();
 		mojo.outputDirectory = new File("target/test/metadata");
-		
+
 		assertDoesNotThrow(mojo::execute);
+	}
+
+	@Test
+	void handleDescriptionReadException() throws Exception {
+		ErrorAwayRulesMojo mojo = spy(ErrorAwayRulesMojo.class);
+		mojo.outputDirectory = new File("target/test/metadata");
+
+		when(mojo.findDescriptionResource(anyString())).thenReturn(new URL("file://doesnotexist"));
+
+		assertThatCode(mojo::execute).hasMessageStartingWith("Error parsing MD description");
+
+		verify(mojo, times(1)).handleDescriptionReadException(anyString(), any());
 	}
 }
